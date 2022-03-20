39b943b6dd70af99a7ef222bd505a82d04a94ec3
mathew4STAR <mathewph249@gmail.com>
2022-03-20 14:55:45 +0530

    major changes

--- eyedetector.py
+++ eyedetector.py
@@ -7,58 +7,71 @@ import mediapipe as mp
 mp_face_mesh = mp.solutions.face_mesh
 cap = cv2.VideoCapture(0)
 
-LEFT_EYE = [ 362, 382, 381, 381, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385, 384, 398]
-RIGHT_EYE = [33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161, 246]
-LEFT_IRIS = [474, 475, 476, 477]
-RIGHT_IRIS = [469, 470, 471, 472]
-with mp_face_mesh.FaceMesh(max_num_faces = 1, refine_landmarks = True, min_detection_confidence = 0.5, min_tracking_confidence = 0.5) as face_mesh:
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        frame = cv2.flip(frame, 1)
-        img_height, img_width  = frame.shape[:2]
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = face_mesh.process(rgb_frame)
-        if results.multi_face_landmarks:
-            #print(results.multi_face_landmarks)
-            mesh_points = np.array([np.multiply([p.x, p.y], [img_width, img_height]).astype(int) for p in results.multi_face_landmarks[0].landmark])
-            #print(mesh_points.shape)
-            cv2.polylines(frame, [mesh_points[LEFT_EYE]], True, (0, 255, 0), 1, cv2.LINE_AA)
-            cv2.polylines(frame, [mesh_points[RIGHT_EYE]], True, (0, 255, 0), 1, cv2.LINE_AA)
-            cv2.polylines(frame, [mesh_points[LEFT_IRIS]], True, (0, 255, 0), 1, cv2.LINE_AA)
-            cv2.polylines(frame, [mesh_points[RIGHT_IRIS]], True, (0, 255, 0), 1, cv2.LINE_AA)
-            for i in results.multi_face_landmarks:
-                #print(i.landmark[42].y * 480)
-                pos_4 = i.landmark[4].y * 480
-                pos_19 = i.landmark[19].y * 480
-                dis = pos_19 - pos_4
-                #if dis > 8:
-                    #print("yes")
-                #else:
-                    #print("no")
-                """
-                cv2.circle(frame, (int(i.landmark[4].x * 640) , int(i.landmark[4].y * 480)), 5, (0, 255, 0), -1)
-                cv2.circle(frame, (int(i.landmark[19].x * 640) , int(i.landmark[19].y * 480)), 5, (0, 255, 0), -1)
-                cv2.circle(frame, (int(i.landmark[474].x * 640) , int(i.landmark[474].y * 480)), 5, (0, 255, 0), -1)
-                cv2.circle(frame, (int(i.landmark[466].x * 640) , int(i.landmark[466].y * 480)), 5, (0, 255, 0), -1)
-                cv2.circle(frame, (int(i.landmark[471].x * 640) , int(i.landmark[471].y * 480)), 5, (0, 255, 0), -1)
-                cv2.circle(frame, (int(i.landmark[33].x * 640) , int(i.landmark[33].y * 480)), 5, (0, 255, 0), -1)
-                """
-                #Left Eye
-                pos_474 = i.landmark[474].x * 640
-                pos_466 = i.landmark[466].x * 640
-                #Right Eye
-                pos_471 = i.landmark[471].x * 640
-                pos_33 = i.landmark[33].x * 640
+def track(show_landmarks):
+    iris_status = ""
+    head_status = ""
 
-                if pos_466 - pos_474 < 2:
-                    print("Looking Left")
-                if pos_471 - pos_33 < 2:
-                    print("Looking Right")
-        cv2.imshow('Eye Tracker', frame)
-        key = cv2.waitKey(1)
-        if key == ord('q'):
-            break
-cap.release()
-cv2.destroyAllWindows()
+    LEFT_EYE = [ 362, 382, 381, 381, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385, 384, 398]
+    RIGHT_EYE = [33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161, 246]
+    LEFT_IRIS = [474, 475, 476, 477]
+    RIGHT_IRIS = [469, 470, 471, 472]
+    with mp_face_mesh.FaceMesh(max_num_faces = 1, refine_landmarks = True, min_detection_confidence = 0.5, min_tracking_confidence = 0.5) as face_mesh:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frame = cv2.flip(frame, 1)
+            img_height, img_width  = frame.shape[:2]
+            rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            results = face_mesh.process(rgb_frame)
+            if results.multi_face_landmarks:
+                #print(results.multi_face_landmarks)
+                mesh_points = np.array([np.multiply([p.x, p.y], [img_width, img_height]).astype(int) for p in results.multi_face_landmarks[0].landmark])
+                #print(mesh_points.shape)
+                if show_landmarks == True:
+                    cv2.polylines(frame, [mesh_points[LEFT_EYE]], True, (0, 255, 0), 1, cv2.LINE_AA)
+                    cv2.polylines(frame, [mesh_points[RIGHT_EYE]], True, (0, 255, 0), 1, cv2.LINE_AA)
+                    cv2.polylines(frame, [mesh_points[LEFT_IRIS]], True, (0, 255, 0), 1, cv2.LINE_AA)
+                    cv2.polylines(frame, [mesh_points[RIGHT_IRIS]], True, (0, 255, 0), 1, cv2.LINE_AA)
+                for i in results.multi_face_landmarks:
+                    #print(i.landmark[42].y * 480)
+                    pos_4 = i.landmark[4].y * 480
+                    pos_19 = i.landmark[19].y * 480
+                    dis = pos_19 - pos_4
+                    if dis > 8:
+                        head_status = "Head is looking at the camera"
+                    else:
+                        head_status = "Head is looking at the screen"
+                    if show_landmarks == True:
+                        cv2.circle(frame, (int(i.landmark[4].x * 640) , int(i.landmark[4].y * 480)), 5, (0, 255, 0), -1)
+                        cv2.circle(frame, (int(i.landmark[19].x * 640) , int(i.landmark[19].y * 480)), 5, (0, 255, 0), -1)
+                        cv2.circle(frame, (int(i.landmark[474].x * 640) , int(i.landmark[474].y * 480)), 5, (0, 255, 0), -1)
+                        cv2.circle(frame, (int(i.landmark[466].x * 640) , int(i.landmark[466].y * 480)), 5, (0, 255, 0), -1)
+                        cv2.circle(frame, (int(i.landmark[471].x * 640) , int(i.landmark[471].y * 480)), 5, (0, 255, 0), -1)
+                        cv2.circle(frame, (int(i.landmark[33].x * 640) , int(i.landmark[33].y * 480)), 5, (0, 255, 0), -1)
+                    
+                    #Left Eye
+                    pos_474 = i.landmark[474].x * 640
+                    pos_466 = i.landmark[466].x * 640
+                    #Right Eye
+                    pos_471 = i.landmark[471].x * 640
+                    pos_33 = i.landmark[33].x * 640
+
+                    if pos_466 - pos_474 < 2:
+                        iris_status = "Left Left"
+                    elif pos_471 - pos_33 < 2:
+                        iris_status = "Looking Right"
+                    else:
+                        iris_status = "Looking Straight"
+                    
+                    return head_status, iris_status, frame
+            '''''
+            cv2.imshow('Eye Tracker', frame)
+            key = cv2.waitKey(1)
+            if key == ord('q'):
+                break
+            '''''
+'''''
+    cap.release()
+    cv2.destroyAllWindows()
+'''''
---
+++ main.py
@@ -0,0 +1,29 @@
+import eyedetector
+from tkinter import *
+import cv2
+from PIL import Image, ImageTk
+
+root = Tk()
+title = Label(root, text="Interview Trainer")
+title.pack()
+question = Label(root, text= "Question 1 this is very cool who are you?")
+question.pack()
+nxtquestion = Button(root)
+nxtquestion.pack()
+screen1 = LabelFrame(root)
+screen1.pack()
+screen2 = Label(screen1)
+screen2.pack()
+focus = Label(root, text="starting")
+iris = Label(root, text="starting")
+focus.pack()
+iris.pack()
+
+while True:
+    tracker = eyedetector.track(False)
+    img = ImageTk.PhotoImage(Image.fromarray(tracker[2]))
+    screen2["image"] = img
+    focus["text"] = tracker[0]
+    iris["text"] = tracker[1]
+    root.update()
+cv2.destroyAllWindows()
